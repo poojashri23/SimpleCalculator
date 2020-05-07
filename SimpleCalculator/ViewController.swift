5be15987ec1ead10aa5fec72cde04591ac12e0ce
Pooja <pooja.foujadar@symphonyfintech.in>
2020-05-07 18:21:42 +0530

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Pooja Foujadar on 07/05/20.
+//  Copyright © 2020 Pooja Foujadar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
